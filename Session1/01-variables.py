e3c52787312335ee6364a9c185cacdfe1cf23c7e
Oscar Perez <88859720+oscar-steven@users.noreply.github.com>
2021-12-10 20:10:08 -0500

    chore: declaracion de variable

---
+++ Session1/01-variables.py
@@ -0,0 +1,43 @@
+# variavle numerica
+numero = 10
+numero2 = 10.5
+
+numero=50
+
+
+# variables de texto o string
+nombre = "Oscar"
+apellido = "Ramiro"
+
+html="""<html>
+<p>
+</p>"""
+
+print("holaaaa :)")
+print(type(nombre))
+
+#str = string
+#int = integer
+#float = float
+print(type(numero))
+print(type(numero2))
+#bool = boolean
+soltero = True
+calor = False
+
+print(type(soltero))
+
+# VARIABLES que tienen varios VALORES
+# ARREGLOS > LISTAS LIST
+
+edades = [10,12 ,40, 60, 'Eduardo', 14.5, False, [1,2]]
+
+print(type(edades))
+
+# JSON (JavaScript Objet Notation) | Diccionario
+curso = {
+    'nombre': 'Backend',
+    'dificultad': 'Dificil'
+}
+
+print(curso)
