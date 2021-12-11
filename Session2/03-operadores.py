1311dd55797c0b20de160c9570f492b18056fbd2
Oscar Perez <88859720+oscar-steven@users.noreply.github.com>
2021-12-11 16:21:48 -0500

    chore: operadores mayor o igual

---
+++ Session2/03-operadores.py
@@ -0,0 +1,26 @@
+# Operadores de asignacion
+# Igual =
+# Incremento +=
+# Decremento -=
+# Multiplicacion
+
+numero = 100
+numero +=5
+
+print(numero)
+
+# Operadores de comparacion
+# == para saer si son el mismo valor
+# != para saber si el somn diferentes
+# <,> para saber si es menor o mayor que
+
+print(5 == 5)
+
+# Operadores Logicos
+# and =
+# or
+# not
+# EN JS el and es &&
+# EN JS el or es ||
+# EN JS el not es !
+print((10<20) and (50>80))
