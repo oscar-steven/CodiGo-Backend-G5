6fd58a84476704e23a1445f0761e801974f1b3fe
Oscar Perez <88859720+oscar-steven@users.noreply.github.com>
2022-01-08 17:20:25 -0500

    chore:mysql

--- Sesion03/simulador.py
+++ Sesion03/simulador.py
@@ -11,6 +11,17 @@ objFaker.add_provider(internet)
 
 cursos = ['COMUNICACION', 'CTA', 'INGLES', 'FRENCH']
 
+# Para no repetir en una lista.
+data = [[1,3],[10,2],[32,1],[55,4],[86,3],[10,1]]
+x = 0
+while x < 200:
+    curso = objFaker.random_int(min=1, max=4)
+    alumno = objFaker.random_int(min=1, max=100)
+    if [alumno, curso] not in data:
+        x+=1
+        print(f'INSERT INTO ALUMNOS_CURSOS (ALUMNO_ID, CURSO_ID) VALUES({alumno}, {curso});')
+        data.append([alumno, curso])
+
 
 
 for namecursos in cursos:
