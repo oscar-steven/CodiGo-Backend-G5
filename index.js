be618e99d88dc9dbe45335ad3cd01ce3525b1caa
Oscar Perez <88859720+oscar-steven@users.noreply.github.com>
2022-01-20 19:13:30 -0500

    feat: express

---
+++ index.js
@@ -0,0 +1,19 @@
+import express from "express";
+
+// const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  // req => es la informacion que me viene del liente
+  // res > es la respuesta que le dare al cliente
+  res.status(200).json({
+    message: "Peticion realizada exitosamente",
+  });
+});
+
+// se mantendra escuchando las consultas realizadas a este servidor mediante el puerto definido
+app.listen(port, () => {
+  // esto sucedera cuando se levate el servidor de express
+  console.log(`Servidor levantado exitosamente! ${port}`);
+});
