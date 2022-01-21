12671b6fc09b2831bc9e19a203cf5c56294ebd6f
Oscar Perez <88859720+oscar-steven@users.noreply.github.com>
2022-01-20 22:02:48 -0500

    chore: finalizacion inicio express

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 import express, { json, raw } from "express";
+import cors from "cors";
 
 // const express = require("express");
 const app = express();
-const producto = [];
+const producto = [
+  {
+    nombre: "Leche de almendas",
+    precio: 9.5,
+    estado: true,
+  },
+];
 // middleware > es un intermediario entre todas las peticiones que se realicen a determinado endpoint o si no se indica a todas las peticiones de mi API
 // que mi application de express podra entender toda la informacion enviada por el cliente siempre y cuando sea un json
 app.use(json());
+
+// para toda la aplicacion voy a utilizar la configuracion de los CORS
+app.use(
+  cors()
+  //   { methods: ["GET", "POST"], origin: "http://127.0.0.1:5500/frontend" }
+);
 // app.use(raw());
 const port = 3000;
 
@@ -55,9 +68,57 @@ app.post("/producto", (req, res) => {
 
 app
   .route("/producto/:id")
-  .get((req, res) => {})
-  .put((req, res) => {})
-  .delete((req, res) => {});
+  .get((req, res) => {
+    //destructuracion
+    const { id } = req.params;
+
+    //buscar ese producto por ese id (posicion del array) y si existe, retornar el producto 200
+    // si no existe retornar un estado 200 e indicar en el message que el producto no existe
+
+    console.log(req.params);
+    if (producto[id - 1]) {
+      //si existe
+      return res.status(200).json({
+        content: producto[id - 1],
+      });
+    } else {
+      return res.status(404).json({
+        message: "Producto no existe",
+        content: null,
+      });
+    }
+    // console.log(req.params);
+    // res.json({
+    //   message: null,
+    // });
+  })
+  .put((req, res) => {
+    const { id } = req.params;
+    if (producto[id - 1]) {
+      producto[id - 1] = req.body;
+      return res.status(200).json({
+        message: "Producto actualizado exitosamente",
+        content: producto[id - 1],
+      });
+    } else {
+      return res.status(400).json({
+        message: "Producto no existe",
+        content: null,
+      });
+    }
+  })
+  .delete((req, res) => {
+    const { id } = req.params;
+    if (producto[id - 1]) {
+      const productos = producto[id - 1];
+      producto.splice(id - 1, 1);
+      return res.status(200).json({
+        message: "Producto eliminado exitosamente",
+        content: productos,
+      });
+    } else {
+    }
+  });
 
 // se mantendra escuchando las consultas realizadas a este servidor mediante el puerto definido
 app.listen(port, () => {
