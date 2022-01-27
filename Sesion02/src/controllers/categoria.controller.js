0a6f8c65540985a7b6d7d979afeb317a2dec2e1d
Oscar Perez <88859720+oscar-steven@users.noreply.github.com>
2022-01-26 19:16:18 -0500

    chore: modificaciones

--- Sesion02/src/controllers/categoria.controller.js
+++ Sesion02/src/controllers/categoria.controller.js
@@ -12,3 +12,41 @@ export const listarCategoria = async (req, res) => {
   const categorias = await Prisma.categoria.findMany({});
   return res.json({ content: categorias });
 };
+
+export const buscarCategoria = async (req, res) => {
+  console.log(req.query);
+  const params = req.query;
+  // si en los params hay el estado entonces valiar que si su valor es 'true' entonces sera true caso contrario sera false
+
+  if (params.estado) {
+    params.estado = params.estado == "true" ? true : false;
+  }
+  //SELECT * FROM categoria WHERE nombre= 'Lacteos' and estado = TRUE
+  const categorias = await Prisma.categoria.findMany({ where: params });
+
+  return res.json({
+    content: categorias,
+  });
+};
+
+export const actualizarCategoria = async (req, res) => {
+  const id = +req.params.id;
+  try {
+    await Prisma.categoria.findUnique({ where: { id: id } });
+
+    const categoriaActualizada = await Prisma.categoria.update({
+      data: req.body,
+      where: { id: id },
+    });
+
+    return res.status(200).json({
+      content: categoriaActualizada,
+    });
+  } catch (error) {
+    console.log(error);
+    // se disparara el catch cuando no se encuentre la categoria o cuando la informacion a actualizar sea incorrecta
+    return res.status(400).json({
+      message: "Error al actualizar la categoria",
+    });
+  }
+};
