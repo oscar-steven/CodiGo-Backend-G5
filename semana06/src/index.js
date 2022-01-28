ab6fea31e89ab7ab1aa52c557a4f9d7cfb99fd05
Oscar Perez <88859720+oscar-steven@users.noreply.github.com>
2022-01-27 22:03:51 -0500

    feat: pvalidacion login

--- semana06/src/index.js
+++ semana06/src/index.js
@@ -1,6 +1,16 @@
-import express from "express";
+import express, { json } from "express";
+import morgan from "morgan";
+import { authRouter } from "./routes/auth.routes.js";
 
 const app = express();
+
+app.use(morgan("dev"));
+app.use(json());
+
+// defino mis rutas
+app.use(authRouter);
+// fin de la definicion
+
 const PORT = process.env.PORT ?? 3001;
 
 app.listen(PORT, () => {
--- semana06/src/services/auth.service.js
+++ semana06/src/services/auth.service.js
@@ -1,3 +1,15 @@
+import { prisma } from "../prisma.js";
+
 export class AuthService {
-  static async login() {}
+  static async login({ correo, password }) {
+    // SELECT password, tipo_usuario FROM USUARIO WHERE correo='..'
+    // si no lo encuentra lanzara un error de not found
+    const usuarioEncontrado = await prisma.usuario.findUnique({
+      where: { correo },
+      select: { password: true, tipoUsuario: true },
+      rejectOnNotFound: true,
+    });
+
+    return { message: "Si existe" };
+  }
 }
