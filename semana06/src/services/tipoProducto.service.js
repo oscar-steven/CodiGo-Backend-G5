596866af37d375bc87c7ed0c318e46c12baa59b0
Oscar Perez <88859720+oscar-steven@users.noreply.github.com>
2022-01-28 22:03:43 -0500

    feat: conectando servicio tipo producto

--- semana06/src/services/tipoProducto.service.js
+++ semana06/src/services/tipoProducto.service.js
@@ -2,7 +2,7 @@ import { prisma } from "../prisma.js";
 
 export class TipoProductoService {
   // los metodos staticos son metodos que sirven para utilizarce sin hacer una instancia y de la misma manera, cuando se realice la instancia no se podra acceder a dicho metodo, estos metodos se usan mas que todo para clases abstractas en la cual se usa para plantilla para herencias de otras clases
-  static async crearTipoProducto({ nombreProducto }) {
+  static async crearTipoProducto({ nombreProducto, usuarioId }) {
     const usuarioEncontrado = await prisma.usuario.findUnique({
       where: { id: usuarioId },
     });
