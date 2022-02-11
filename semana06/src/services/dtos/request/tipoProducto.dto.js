fd9e4c52c35dffdd9923cb4a92609df2826bea79
Oscar Perez <88859720+oscar-steven@users.noreply.github.com>
2022-02-10 19:02:35 -0500

    feat: inicio

--- semana06/src/services/dtos/request/tipoProducto.dto.js
+++
@@ -1,9 +0,0 @@
-import validator from "validator";
-
-export function tipoPrudctoDto({ nombre }) {
-  if (validator.isEmpty(nombre)) {
-    throw Error("El nombre no puede ser vacio");
-  }
-
-  return { nombre };
-}
