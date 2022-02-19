c0ae64141960c136bbda6cd3a8397d8478b3fede
Oscar Perez <88859720+oscar-steven@users.noreply.github.com>
2022-02-18 21:53:17 -0500

    feat: integrando mercado pago

--- semana09/src/server.js
+++ semana09/src/server.js
@@ -1,7 +1,16 @@
 import express, { json } from "express";
 import mongoose from "mongoose";
 import { categoriaRouter } from "./routes/categoria.routes.js";
+import { categoriaProductoRouter } from "./routes/categoria_producto.routes.js";
 import { productoRouter } from "./routes/producto.routes.js";
+import mercadopago from "mercadopago";
+
+// access_token > es la token que se va a crear por cada negocio y esto servira a MP para saber a que negocio tiene que depositar el monto cobrado
+// integrator_id > el id del desarrollador que esta haciendo esta integracion
+mercadopago.configure({
+  access_token: process.env.ACCESS_TOKEN_MP,
+  integrator_id: process.env.INTEGRATOR_ID_MP,
+});
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
@@ -10,6 +19,7 @@ app.use(json());
 
 app.use(productoRouter);
 app.use(categoriaRouter);
+app.use(categoriaProductoRouter);
 
 app.listen(PORT, async () => {
   console.log(`Servidor corriendo exitosamente en el puerto ${PORT}`);
