5c7aa26adc54665a0a392faefe89ae351ee5a920
Oscar Perez <88859720+oscar-steven@users.noreply.github.com>
2022-02-24 22:06:32 -0500

    feat: pasarela de pago y cliente

--- semana09/src/server.js
+++ semana09/src/server.js
@@ -4,9 +4,12 @@ import { categoriaRouter } from "./routes/categoria.routes.js";
 import { categoriaProductoRouter } from "./routes/categoria_producto.routes.js";
 import { productoRouter } from "./routes/producto.routes.js";
 import mercadopago from "mercadopago";
+import { pagoRouter } from "./routes/pago.routes.js";
+import { clienteRouter } from "./routes/cliente.routes.js";
 
 // access_token > es la token que se va a crear por cada negocio y esto servira a MP para saber a que negocio tiene que depositar el monto cobrado
 // integrator_id > el id del desarrollador que esta haciendo esta integracion
+
 mercadopago.configure({
   access_token: process.env.ACCESS_TOKEN_MP,
   integrator_id: process.env.INTEGRATOR_ID_MP,
@@ -20,6 +23,8 @@ app.use(json());
 app.use(productoRouter);
 app.use(categoriaRouter);
 app.use(categoriaProductoRouter);
+app.use(pagoRouter);
+app.use(clienteRouter);
 
 app.listen(PORT, async () => {
   console.log(`Servidor corriendo exitosamente en el puerto ${PORT}`);
--- semana09/src/services/pago.service.js
+++ semana09/src/services/pago.service.js
@@ -1,37 +1,78 @@
-import { preferences } from "mercadopago";
+import mercadopago from "mercadopago";
+import { Cliente } from "../models/cliente.model.js";
+import { Producto } from "../models/producto.model.js";
 
 export class PagoService {
-  static async generarPreferenciaDePago() {
+  static async generarPreferenciaDePago({ items, cliente }) {
+    // https://www.mercadopago.com.pe/developers/es/reference/preferences/_checkout_preferences/post
     try {
-      // https://www.mercadopago.com.pe/developers/es/reference/preferences/_checkout_preferences/post
-      preferences.create({
+      // buscar ese cliente
+      const clienteEncontrado = await Cliente.findById(cliente);
+      // buscar esos productos (items)
+
+      // hacer la busqueda de los productos usando el .map()
+      const productosEncontrados = await Promise.all(
+        items.map(async ({ id, cantidad }) => {
+          const productoEncontrado = await Producto.findById(id);
+
+          return { item: productoEncontrado, cantidad };
+        })
+      );
+
+      // early return fuction
+      const itemsMP = productosEncontrados.map(({ item, cantidad }) => ({
+        id: item._id,
+        title: item.nombre,
+        quantity: cantidad,
+        unit_price: Number(item.precio),
+        currency_id: "PEN",
+      }));
+
+      console.log(itemsMP);
+
+      const preferencia = await mercadopago.preferences.create({
         payer: {
-          name: "Oscar", // obligatorio
-          surname: "Perez", //  obligatorio
+          name: clienteEncontrado.nombre, // obligatorio
+          surname: clienteEncontrado.apellido, //  obligatorio
           address: {
-            zip_code: "04002",
-            street_name: "Calle Los Platanitos 123",
-            street_number: "123",
+            zip_code: clienteEncontrado.direccion.zip,
+            street_name: clienteEncontrado.direccion.calle,
+            street_number: clienteEncontrado.direccion.numero,
           },
-          email: "ophuaman@gmail.com",
-          identification: {
-            type: "DNI",
-            number: "48074339",
-          },
+          email: clienteEncontrado.correo,
+          // identification: {
+          //   type: "DNI",
+          //   number: "48074339",
+          // },
         },
-        items: [
-          {
-            id: "1234",
-            title: "zapatillas de running",
-            quantity: 1,
-            unit_price: 115.0,
-            currency_id: "PEN",
-          },
-        ],
+        items: itemsMP,
+        // [
+        //   {
+        //     id: "1234",
+        //     title: "zapatillas de running",
+        //     quantity: 1,
+        //     unit_price: 115.0,
+        //     currency_id: "PEN",
+        //   },
+        // ],
+
+        // Si la transaccion fue exitosa o pendiente de pago entonces nos redireccionara automaticamente a la pagina en cuestion
         auto_return: "approved",
+        // Son las url que me llevaran al sitio si el pago fue:
+        back_urls: {
+          success: "http://localhost:3000/exito",
+          pending: "http://localhost:3000/pendiente",
+          failure: "http://localhost:3000/fallo",
+        },
       });
+      return {
+        resultado: preferencia,
+      };
     } catch (error) {
       console.log(error);
+      return {
+        error: error.message,
+      };
     }
   }
 }
