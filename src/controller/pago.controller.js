d343c3673236cbc5cba2b42e0a354e16c6a8f9b5
Oscar Perez <88859720+oscar-steven@users.noreply.github.com>
2022-02-25 21:29:08 -0500

    chore: modificando query params

--- src/controller/pago.controller.js
+++ src/controller/pago.controller.js
@@ -1,12 +1,16 @@
 import { PagoService } from "../services/pago.service.js";
 
 export async function crearPreferencia(req, res) {
+  // implementar el dto
+
   const resultado = await PagoService.generarPreferenciaDePago(req.body);
+
   return res.status(201).json(resultado);
 }
 
 export function recibirNotificacion(req, res) {
-  console.log("los query params son:"), console.log(re.params);
+  console.log("los query params son:");
+  console.log(req.query);
 
   PagoService.recibirNotificacion(req.body);
 
